84111c2cbe5f9aaec55a2b77ce01a202b6a0afca
SHREYAS M C <140893678+shreyasmc1234@users.noreply.github.com>
2024-12-21 18:28:35 +0530

    Update Customer_who_nexer_order.sql

--- Customer_who_nexer_order.sql
+++ Customer_who_nexer_order.sql
@@ -1,4 +1,12 @@
 select name "Customers" from Customers where id not in
 (select customers.id
-from Customers inner join Orders on customers.id=orders.customerid)
+from Customers inner join Orders on customers.id=orders.customerid);
+
+
+select name as Customers
+from Customers c left join Orders o on c.id=o.customerid
+where o.customerid is null;
+
+
+
 
